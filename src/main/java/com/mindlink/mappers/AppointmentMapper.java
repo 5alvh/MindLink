c5df4f770920b5fc19d29041efb00396e4ef2385
Madtore <143751703+Madtore@users.noreply.github.com>
2025-02-04 11:57:04 +0100

    Update AppointmentMapper.java

--- src/main/java/com/mindlink/mappers/AppointmentMapper.java
+++ src/main/java/com/mindlink/mappers/AppointmentMapper.java
@@ -6,19 +6,20 @@ import com.mindlink.models.dtos.appointmentdtos.AppointmentDTO;
 public class AppointmentMapper {
 
     public static AppointmentDTO appointmentToDTO(Appointment appointment) {
-        // AppointmentDTO dto = new AppointmentDTO(
-        // appointment.getId(),
-        // appointment.getPatient().getEmail(),
-        // appointment.getDoctor().getEmail(),
-        // appointment.getRoom().getRoomUrl(),
-        // appointment.getAppointmentDate(),
-        // appointment.getSessionType(),
-        // appointment.getTotalCost(),
-        // appointment.getRating(),
-        // appointment.getFeedback(),
-        // appointment.getCreatedAt(),
-        // appointment.getUpdatedAt(),
-        // appointment.getDeletedAt() != null);
-        return null;
+        AppointmentDTO dto = new AppointmentDTO(
+                appointment.getId(),
+                appointment.getPatient().getEmail(),
+                appointment.getDoctor().getEmail(),
+                appointment.getRoom().getRoomUrl(),
+                appointment.getAppointmentDate(),
+                appointment.getSessionType(),
+                appointment.getTotalCost(),
+                appointment.getRating(),
+                appointment.getFeedback(),
+                appointment.getCreatedAt(),
+                appointment.getUpdatedAt(),
+                appointment.getDeletedAt() != null);
+        return dto;
     }
 }
+
