435566be0b27541796a80fd974c253cd0a6b853b
Madtore <143751703+Madtore@users.noreply.github.com>
2025-02-04 11:57:29 +0100

    Update RoomMapper.java

--- src/main/java/com/mindlink/mappers/RoomMapper.java
+++ src/main/java/com/mindlink/mappers/RoomMapper.java
@@ -1,9 +1,12 @@
 package com.mindlink.mappers;
 
+import com.mindlink.models.Room;
+import com.mindlink.models.dtos.roomdtos.RoomDTO;
+
 public class RoomMapper {
 
-    // public static RoomDTO roomToDTo(Room room) {
-    // return new RoomDTO("https://meet.jit.si/mindlink-appointment-" +
-    // room.getRoomUrl());
-    // }
+    public static RoomDTO roomToDTo(Room room) {
+        return new RoomDTO("https://meet.jit.si/mindlink-appointment-" +
+                room.getRoomUrl());
+    }
 }
