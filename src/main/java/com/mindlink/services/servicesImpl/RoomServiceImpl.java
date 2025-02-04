c3268b85e6c8370b2af883bd1d1ffebf46691c81
Madtore <73850831+Madtore@users.noreply.github.com>
2025-02-04 12:25:06 +0100

    [FIX] fix RoomServiceImpl

--- src/main/java/com/mindlink/services/servicesImpl/RoomServiceImpl.java
+++ src/main/java/com/mindlink/services/servicesImpl/RoomServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mindlink.mappers.RoomMapper;
 import com.mindlink.models.Appointment;
 import com.mindlink.models.Room;
 import com.mindlink.models.dtos.roomdtos.RoomDTO;
@@ -18,55 +19,35 @@ import jakarta.transaction.Transactional;
 @Service
 public class RoomServiceImpl implements RoomService {
 
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Transactional
     @Override
     public Room createRoom(Appointment appointment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createRoom'");
+        Room room = new Room();
+        room.setRoomUrl(UUID.randomUUID().toString().substring(0, 20));
+        room.setAppointment(appointment);
+        room.setCreatedAt(LocalDate.now());
+        return roomRepository.save(room);
     }
 
     @Override
     public boolean isOpenJointToRoom(Long idAppointment) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isOpenJointToRoom'");
+        Room room = roomRepository.findByAppointmentId(idAppointment)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime appointmentDateTime = room.getAppointment().getAppointmentDate();
+
+        return now.isAfter(appointmentDateTime) &&
+                now.isBefore(appointmentDateTime.plusHours(1));
     }
 
     @Override
     public RoomDTO findByAppointmentId(Long appointmentID) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findByAppointmentId'");
+        return roomRepository.findByAppointmentId(appointmentID).map(RoomMapper::roomToDTo)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
     }
-
-    // @Autowired
-    // private RoomRepository roomRepository;
-
-    // @Transactional
-    // @Override
-    // public Room createRoom(Appointment appointment) {
-    // Room room = new Room();
-    // // room.setRoomUrl(UUID.randomUUID().toString().substring(0, 20));
-    // // room.setAppointment(appointment);
-    // // room.setCreatedAt(LocalDate.now());
-    // return roomRepository.save(room);
-    // }
-
-    // @Override
-    // public boolean isOpenJointToRoom(Long idAppointment) {
-    // Room room = roomRepository.findByAppointmentId(idAppointment)
-    // .orElseThrow(() -> new RuntimeException("Room not found"));
-
-    // LocalDateTime now = LocalDateTime.now();
-    // LocalDateTime appointmentDateTime =
-    // room.getAppointment().getAppointmentDate();
-
-    // return now.isAfter(appointmentDateTime) &&
-    // // now.isBefore(appointmentDateTime.plusHours(1));
-    // }
-
-    // @Override
-    // public RoomDTO findByAppointmentId(Long appointmentID) {
-    // return
-    // roomRepository.findByAppointmentId(appointmentID).map(RoomMapper::roomToDTo)
-    // .orElseThrow(() -> new RuntimeException("Room not found"));
-    // }
 
 }
